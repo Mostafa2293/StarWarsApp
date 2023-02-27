acdf41152fea18dc2f53993e3756ac52e2144e62
mostafa mohamed <mostafa.mohamed@acmesaico.com>
2023-02-27 14:55:09 +0200

    people speech button

--- app/src/main/java/com/mostafa/starwarsfans/presentation/people/PeopleFragment.kt
+++ app/src/main/java/com/mostafa/starwarsfans/presentation/people/PeopleFragment.kt
@@ -1,11 +1,16 @@
 package com.mostafa.starwarsfans.presentation.people
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
@@ -15,16 +20,19 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
 import com.mostafa.starwarsfans.R
 import com.mostafa.starwarsfans.databinding.FragmentPeopleBinding
+import com.mostafa.starwarsfans.presentation.SearchInterface
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 @AndroidEntryPoint
 class PeopleFragment : Fragment() {
 
     private val viewModel: PeopleViewModel by viewModels()
     lateinit var binding:FragmentPeopleBinding
+    private val requestCodeSpeech = 102
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -65,6 +73,36 @@ class PeopleFragment : Fragment() {
 
 
         })
+        binding.searchButton.setOnClickListener(View.OnClickListener {
+            Log.d("button", "onSearchBtnClicked: Clicked!!")
+            requestSpeechInput()
+        })
 
+    }
+
+/*    override fun onSearchBtnClicked() {
+        Log.d("button", "onSearchBtnClicked: Clicked!!")
+        requestSpeechInput()
+    }*/
+
+    private fun requestSpeechInput() {
+        if(!SpeechRecognizer.isRecognitionAvailable(requireActivity())){
+            Toast.makeText(requireActivity(),"speech recognition is not available",Toast.LENGTH_SHORT).show()
+        }else{
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,Locale.getDefault())
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say Something !")
+            startActivityForResult(intent,requestCodeSpeech)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==requestCodeSpeech && resultCode == Activity.RESULT_OK){
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+//            viewModel.getPeople(result!![0].toString())
+            binding.searchBar.setQuery(result!![0].toString(),true)
+        }
     }
 }
